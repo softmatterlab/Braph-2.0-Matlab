2c7924e0de44a5d7a46a8e8f385709d5f079e6fa
Yu-Wei Chang <hejyuwei@gmail.com>
2021-01-22 14:00:40 +0100

    Create _YW_Trial.gen.m
    init Trial practice

---
+++ braph2genesis/src/ds/_YW_Trial.gen.m
@@ -0,0 +1,24 @@
+%% ¡header!
+YW_Trial < Element (yw_tr, trial element) is a trial element.
+
+%% ¡props!
+
+%%% ¡prop!
+a (data, scalar) is a.
+
+%%% ¡prop!
+b (data, scalar) is b.
+
+%%% ¡prop!
+res (result, scalar) is result (a - b).
+
+%%%% ¡calculate!
+value = yw_tr.get('A') - yw_tr.get('B')
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+
+
+
