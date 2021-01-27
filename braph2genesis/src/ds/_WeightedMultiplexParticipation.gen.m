e29db465c12d257ebcdb597ad16b0a0777fd721c
annacanal <annacg@kth.se>
2021-01-27 23:31:59 +0100

    add test to weighted multiplex participation

--- braph2genesis/src/ds/_WeightedMultiplexParticipation.gen.m
+++ braph2genesis/src/ds/_WeightedMultiplexParticipation.gen.m
@@ -33,3 +33,34 @@ participation = L / (L - 1) * (1 - participation);
 %weighted_multiplex_participation(isnan(weighted_multiplex_participation)) = 0;  % Should return zeros, since NaN happens when strength = 0 and overlapping strength = 0 for all regions
 WMPC = {participation}
 value = WMPC;
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexGraphWU
+%%%% ¡code!
+A11 = [
+    0   .5  1
+    .5  0   0
+    1   0   0
+    ];
+A12 = eye(3);
+A21 = eye(3);
+A22 = [
+    0   1   0
+    1   0   .5
+    0   .5  0
+    ];
+A = {
+    A11     A12
+    A21     A22
+    };
+
+known_weighted_multiplex_participation = {[24/25 3/4 8/9]'};
+          
+weighted_multiplex_participation = WeightedMultiplexParticipation('a', A);
+
+assert(isequal(weighted_multiplex_participation.get('WMPC'), known_weighted_multiplex_participation), ...
+    [BRAPH2.STR ':WeightedMultiplexParticipation:' BRAPH2.BUG_ERR], ...
+    'WeightedMultiplexParticipation is not being calculated correctly for MultiplexGraphWU.')
